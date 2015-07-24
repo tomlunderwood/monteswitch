75592650b29b97ec8c42f3a0cca7bb1699070808
Tom Underwood <t.l.underwood@ed.ac.uk>
2015-07-24 17:22:27 +0100

    interactions.f95 now corresponds to finished EAM version

--- interactions.f95
+++ interactions.f95
@@ -23,19 +23,62 @@
   !! <code>include</code>d in the <code>monteswitch_mod</code> module.
   !! </p>
   !! <p>
-  !! This version of the file corresponds to the embedded atom model.
+  !! This version of the file implements the embedded atom model (EAM) similarly to LAMMPS, as described at
+  !! http://lammps.sandia.gov/doc/pair_eam.html. The input file which specifies the potential (the 'interactions_in'
+  !! file read by monteswitch) must be in the DYNAMO/LAMMPS 'setfl' format; a file with this format is often
+  !! indicated with the suffix '.eam.alloy'. A description of the 'setfl' format can be found at 
+  !! http://lammps.sandia.gov/doc/pair_eam.html. However, note that, since monteswitch currently
+  !! only supports unary systems, the file must not correspond to a multicomponent system - an error is returned
+  !! if this is the case. Note also this file implements the EAM potential by using linear interpolation, using the
+  !! tabulations of the various functions in the input file (the embedding function, the density function, 
+  !! and the pair-potential) as a basis. At initialisation three files are created, 'F.dat', 'rho.dat', and 'rphi.dat', which
+  !! correspond to the embedding function, density function, and pair potential (multiplied by separation) read
+  !! from the input file.
   !! </p>
-  !! <p>
-  !! The format of the file to import the interactions parameters is as follows... (DYNAMO multi-element setfl format,
-  !! but only one element must be present).
-  !! </p>
-
-
 
   !! <h2> 'Interation variables' which are required for initialisation of the particle interactions. </h3>
-  !! <p>
-  !! The following variables are... 
+  !! The following variables are read from the <code>filename_params</code> file by 
+  !! <code>initialise_from_files(filename_params,filename_lattice)</code> in <code>monteswitch_mod</code>.
+  !! These varaibles are later used to initialise how the particles interact.
   !! </p>
+  !! <table border="1">
+  !! <tr>
+  !!  <td> <b> Variable </b> </td>
+  !!  <td> <b> Type </b> </td>
+  !!  <td> <b> Description </b> </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>Nrho<code> </td>
+  !!  <td> <code>integer(ik)</code> </td>
+  !!  <td> The number of points on the 'density grid'. </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>drho<code> </td>
+  !!  <td> <code>real(rk)</code> </td>
+  !!  <td> The spacing between points on the 'density grid'. </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>Nr<code> </td>
+  !!  <td> <code>integer(ik)</code> </td>
+  !!  <td> 
+  !!  The number of points on the 'separation grid'.
+  !!  </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>dr<code> </td>
+  !!  <td> <code>real(rk)</code> </td>
+  !!  <td> 
+  !!  The spacing between points on the 'separation grid'.
+  !!  </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>cutoff<code> </td>
+  !!  <td> <code>real(rk)</code> </td>
+  !!  <td> 
+  !!  The interaction cut-off: pairs of particles beyond this separation do not interact.
+  !!  </td>
+  !! </tr>
+  !! </table> 
   integer(ik) :: Nrho
   real(rk) :: drho
   integer(ik) :: Nr
@@ -45,13 +88,41 @@
 
   !! <h2> Further 'interaction variables'</h3>
   !! <p>
-  !! The following variables are initialised according to the values of the above variables...
+  !! The following variables are initialised according to the values of the above variables.
   !! </p>
-  ! Embedding function (function of density rho)
+  !! <table border="1">
+  !! <tr>
+  !!  <td> <b> Variable </b> </td>
+  !!  <td> <b> Type </b> </td>
+  !!  <td> <b> Description </b> </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>F<code> </td>
+  !!  <td> <code>real(rk), dimension(:), allocatable</code> </td>
+  !!  <td>
+  !!  Array defining the embedding function: <code>F(i)</code> is the embedding function at density
+  !!  <code>(i-1)*drho</code>.
+  !!  </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>rho<code> </td>
+  !!  <td> <code>real(rk), dimension(:), allocatable</code> </td>
+  !!  <td>
+  !!  Array defining the density function: <code>rho(i)</code> is the density for inter-particle separation
+  !!  <code>(i-1)*dr</code>.
+  !!  </td>
+  !! </tr>
+  !! <tr>
+  !!  <td> <code>rphi<code> </td>
+  !!  <td> <code>real(rk), dimension(:), allocatable</code> </td>
+  !!  <td>
+  !!  Array defining the pair-potential function: <code>rphi(i)</code> is the pair-potential, multiplied by
+  !!  the separation, for inter-particle separation <code>(i-1)*dr</code>.
+  !!  </td>
+  !! </tr>
+  !! </table>
   real(rk), dimension(:), allocatable :: F
-  ! Density function (function of separation r)
   real(rk), dimension(:), allocatable :: rho
-  ! Pair potential function times r (function of separation r)
   real(rk), dimension(:), allocatable :: rphi
 
 
@@ -82,9 +153,12 @@ contains
   !! </table>
   subroutine initialise_interactions(filename)
     character(*), intent(in) :: filename
-    character(len=20) :: string
+    character(len=200) :: line
     integer(ik) :: error
     integer(ik) :: i,j,n
+
+    integer(ik) :: indx, prev_indx
+    integer(ik) :: start_of_word
 
     ! Open the file and import the variables required for initialisation
     open(unit=10,file=filename,iostat=error,status="old")
@@ -94,13 +168,26 @@ contains
     end if
     
     ! Read 3 comment lines
-    read(10,*) string
-    read(10,*) string
-    read(10,*) string
-    ! Read the number of elements and the element name (number of elements must be 1)
-    read(10,iostat=error) n,string
+    read(10,*,iostat=error) line
     if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading line 4 from file '",filename,"'"
+       write(0,*) "interactions: Error reading file '",filename,"'" 
+       stop 1
+    end if
+    read(10,*,iostat=error) line
+    if(error/=0) then
+       write(0,*) "interactions: Error reading file '",filename,"'"
+       stop 1
+    end if
+    read(10,*,iostat=error) line
+    if(error/=0) then
+       write(0,*) "interactions: Error reading file '",filename,"'"
+       stop 1
+    end if
+
+    ! Read the number of elements (number of elements must be 1), and ignoring the element name
+    read(10,*,iostat=error) n
+    if(error/=0) then
+       write(0,*) "interactions: Error reading file '",filename,"'"
        stop 1
     end if
     if(n/=1) then
@@ -108,45 +195,158 @@ contains
             "multicomponent system; only unary systems supported."
        stop 1
     end if
+
     ! Read Nrho, drho, Nr, dr and cutoff
-    read(10,iostat=error) Nrho,drho,Nr,dr,cutoff
+    read(10,*,iostat=error) Nrho,drho,Nr,dr,cutoff
     if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading line 5 (Nrho, drho, Nr, dr, ", &
-            "cutoff) from file '",filename,"'"
+       write(0,*) "interactions: Error reading file '",filename,"'"
        stop 1
     end if
+
     ! Read, but ignore, the line containing the atomic number, mass, lattice constant and lattice type
-    read(10,iostat=error) string
+    read(10,*,iostat=error) line
     if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading line 6 (atomic number, mass, ", &
-            "lattice constant, lattice type) from file '",filename,"'"
+       write(0,*) "interactions: Error reading file '",filename,"'"
        stop 1
     end if
+
     ! Initialise and read the embedding function
     allocate(F(Nrho))
-    read(10,iostat=error) F
-    if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading embedding function from file '",filename,"'"
-       stop 1
-    end if
+    j=1
+    do
+       read(10,'(A)',iostat=error) line
+       if(error/=0) then
+          write(0,*) "interactions: Error reading file '",filename,"'"
+          stop 1
+       end if
+
+       start_of_word=1
+       prev_indx=0
+
+       do i=1,len(line)
+          ! Check if the current character in the line corresponds to a number (i.e., not a space).
+          ! If so, then indx is NOT 0. If indx==0 then we have a blank
+          indx = index('0123456789.-+Ee', line(i:i))
+
+          ! We are at the start of a word if the current character is not a blank, but the previous
+          ! was.
+          if(indx/=0 .and. prev_indx==0) then
+             start_of_word=i
+          end if
+          ! We are at the end of a word if the current character is a blank, but the previous
+          ! was not.
+          if(indx==0 .and. prev_indx/=0) then
+             read(line(start_of_word:i-1), *) F(j)
+             j=j+1
+          end if
+
+          if(j>Nrho) exit
+          
+          prev_indx=indx
+
+       end do
+
+       if(j>Nrho) exit
+
+    end do
+
     ! Initialise and read the density function
-    allocate(rho(Nrho))
-    read(10,iostat=error) rho
-    if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading density function from file '",filename,"'"
-       stop 1
-    end if
+    allocate(rho(Nr))
+    j=1
+    do
+       read(10,'(A)',iostat=error) line
+       if(error/=0) then
+          write(0,*) "interactions: Error reading file '",filename,"'"
+          stop 1
+       end if
+
+       start_of_word=1
+       prev_indx=0
+
+       do i=1,len(line)
+          ! Check if the current character in the line corresponds to a number (i.e., not a space).
+          ! If so, then indx is NOT 0. If indx==0 then we have a blank
+          indx = index('0123456789.-+Ee', line(i:i))
+
+          ! We are at the start of a word if the current character is not a blank, but the previous
+          ! was.
+          if(indx/=0 .and. prev_indx==0) then
+             start_of_word=i
+          end if
+          ! We are at the end of a word if the current character is a blank, but the previous
+          ! was not.
+          if(indx==0 .and. prev_indx/=0) then
+             read(line(start_of_word:i-1), *) rho(j)
+             j=j+1
+          end if
+
+          if(j>Nr) exit
+          
+          prev_indx=indx
+
+       end do
+
+       if(j>Nr) exit
+
+    end do
+    
     ! Initialise and read the pair potential function. Note that the values in the file are actually r*phi(r),
     ! not phi(r). This is stored in the variable rphi.
-    allocate(rphi(Nrho))
-    read(10,iostat=error) rphi
-    if(error/=0) then
-       write(0,*) "interactions: Error. Problem reading pair-potential function from file '",filename,"'"
-       stop 1
-    end if
-    close(10)
+    allocate(rphi(Nr))
+    j=1
+    do
+       read(10,'(A)',iostat=error) line
+       if(error/=0) then
+          write(0,*) "interactions: Error reading file '",filename,"'"
+          stop 1
+       end if
 
-    ! Initialise the interaction variables accordingly
+       start_of_word=1
+       prev_indx=0
+
+       do i=1,len(line)
+          ! Check if the current character in the line corresponds to a number (i.e., not a space).
+          ! If so, then indx is NOT 0. If indx==0 then we have a blank
+          indx = index('0123456789.-+Ee', line(i:i))
+
+          ! We are at the start of a word if the current character is not a blank, but the previous
+          ! was.
+          if(indx/=0 .and. prev_indx==0) then
+             start_of_word=i
+          end if
+          ! We are at the end of a word if the current character is a blank, but the previous
+          ! was not.
+          if(indx==0 .and. prev_indx/=0) then
+             read(line(start_of_word:i-1), *) rphi(j)
+             j=j+1
+          end if
+
+          if(j>Nr) exit
+          
+          prev_indx=indx
+
+       end do
+
+       if(j>Nr) exit
+
+    end do
+
+    ! Output the functions to files
+    open(unit=10,file="F.dat")
+    do i=1,Nrho
+       write(10,*) (i-1)*drho,F(i)
+    end do
+    close(10)
+    open(unit=10,file="rho.dat")
+    do i=1,Nr
+       write(10,*) (i-1)*dr,rho(i)
+    end do
+    close(10)
+    open(unit=10,file="rphi.dat")
+    do i=1,Nr
+       write(10,*) (i-1)*dr,rphi(i)
+    end do
+    close(10)
 
   end subroutine initialise_interactions
 
@@ -453,18 +653,35 @@ contains
 
 
 
-  ! The embedding function for a given rho, according to the array F, and the
-  ! 'rho-grid' defined by Nrho and drho
+  !! <h3> <code> function F_func(rho) </code> </h3>
+  !! <p>
+  !! <code>F_func(rho) </code> returns the embedding energy for a given density,
+  !! given the array <code>F</code>, and the 'density grid' defined by <code>Nrho</code>
+  !! and <code>drho</code>. Note that linear interpolation is used.
+  !! </p>
+  !! <table border="1">
+  !!  <tr>
+  !!   <td> <b> Argument </b> </td>
+  !!   <td> <b> Type </b> </td>
+  !!   <td> <b> Description </b> </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code> rho </code> </td>
+  !!   <td> <code> real(rk) </code> </td>
+  !!   <td> The electron density. </td>
+  !!  </tr>
+  !! </table>
+  !! <p><b>Returns:</b> <code> real(rk) </code> </p>
   function F_func(rho)
     real(rk), intent(in) :: rho
     real(rk) :: F_func
     real(rk) :: bin_cont
     integer(ik) :: bin_below, bin_above
-    ! F(bin) corresponds to the embedding function at rho=bin*drho
-    ! Get the 'continuous' bin number
-    bin_cont=rho/drho
+    ! F(bin) corresponds to the embedding function at rho=(bin-1)*drho.
+    ! Hence bin=rho/drho+1 is the 'continuous' bin number
+    bin_cont=rho/drho+1
     if(bin_cont<1 .or. bin_cont>Nrho) then
-       write(0,*) "interactions_mod: Error in F_func: r not supported"
+       write(0,*) "interactions_mod: Error in F_func: rho not supported. rho = ",rho
        stop 1
     end if
     ! Perform a linear interpolation to get the value of the embedding function at rho
@@ -474,18 +691,37 @@ contains
   end function F_func
 
 
-  ! The density function for a given r, according to the array rho, and the
-  ! 'r-grid' defined by Nr and dr
+
+ 
+  !! <h3> <code> function rho_func(rho) </code> </h3>
+  !! <p>
+  !! <code>rho_func(r) </code> returns the density corresponding to a given separation,
+  !! given the array <code>rho</code>, and the 'separation grid' defined by <code>Nr/code>
+  !! and <code>dr</code>. Note that linear interpolation is used.
+  !! </p>
+  !! <table border="1">
+  !!  <tr>
+  !!   <td> <b> Argument </b> </td>
+  !!   <td> <b> Type </b> </td>
+  !!   <td> <b> Description </b> </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code>r </code> </td>
+  !!   <td> <code> real(rk) </code> </td>
+  !!   <td> The distance between two particles. </td>
+  !!  </tr>
+  !! </table>
+  !! <p><b>Returns:</b> <code> real(rk) </code> </p>
   function rho_func(r)
     real(rk), intent(in) :: r
     real(rk) :: rho_func
     real(rk) :: bin_cont
     integer(ik) :: bin_below, bin_above
-    ! rho(bin) corresponds to the density function at r=bin*dr
-    ! Get the 'continuous' bin number
-    bin_cont=r/dr
+    ! rho(bin) corresponds to the density function at r=(bin-1)*dr.
+    ! Hence bin=r/dr+1 is the 'continuous' bin number
+    bin_cont=r/dr+1
     if(bin_cont<1 .or. bin_cont>Nr) then
-       write(0,*) "interactions_mod: Error in rho_func: r not supported"
+       write(0,*) "interactions_mod: Error in rho_func: r not supported. r = ",r
        stop 1
     end if
     ! Perform a linear interpolation to get the value of the density function at r
@@ -495,18 +731,37 @@ contains
   end function rho_func
 
 
-  ! The phi function for a given r, according to the array rphi, and the
-  ! 'r-grid' defined by Nr and dr
+
+ 
+  !! <h3> <code> function phi_func(rho) </code> </h3>
+  !! <p>
+  !! <code>phi_func(r) </code> returns the value of the pair-potential corresponding to a given separation,
+  !! given the array <code>rphi</code>, and the 'separation grid' defined by <code>Nr/code>
+  !! and <code>dr</code>. Note that linear interpolation is used.
+  !! </p>
+  !! <table border="1">
+  !!  <tr>
+  !!   <td> <b> Argument </b> </td>
+  !!   <td> <b> Type </b> </td>
+  !!   <td> <b> Description </b> </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code>r </code> </td>
+  !!   <td> <code> real(rk) </code> </td>
+  !!   <td> The distance between two particles. </td>
+  !!  </tr>
+  !! </table>
+  !! <p><b>Returns:</b> <code> real(rk) </code> </p>
   function phi_func(r)
     real(rk), intent(in) :: r
     real(rk) :: phi_func
     real(rk) :: bin_cont
     integer(ik) :: bin_below, bin_above
-    ! rphi(bin) corresponds to the value of r*phi at r=bin*dr
-    ! Get the 'continuous' bin number
-    bin_cont=r/dr
+    ! rphi(bin) corresponds to the value of r*phi at r=(bin-1)*dr
+    ! Hence bin=r/dr+1 is the 'continuous' bin number
+    bin_cont=r/dr+1
     if(bin_cont<1 .or. bin_cont>Nr) then
-       write(0,*) "interactions_mod: Error in rho_func: r not supported"
+       write(0,*) "interactions_mod: Error in rho_func: r not supported. r = ",r
        stop 1
     end if
     ! Perform a linear interpolation to get the value of the phi function at r
@@ -517,8 +772,55 @@ contains
   end function phi_func
 
 
-  ! The total energy associated with atom i, given the particle positions and supercell
-  ! dimensions
+
+
+  !! <h3> <code> function particle_energy(Lx,Ly,Lz,r,i) </code> </h3>
+  !! <p>
+  !! <code>particle_energy(Lx,Ly,Lz,r,i) </code> returns the total EAM energy associated
+  !! with particle <code>i</code>, given the specified particle positions and supercell 
+  !! dimensions.
+  !! </p>
+  !! <table border="1">
+  !!  <tr>
+  !!   <td> <b> Argument </b> </td>
+  !!   <td> <b> Type </b> </td>
+  !!   <td> <b> Description </b> </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code> Lx </code> </td>
+  !!   <td> <code> real(rk), intent(in) </code> </td>
+  !!   <td>
+  !!   Length of supercell in x direction.
+  !!   </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code> Ly </code> </td>
+  !!   <td> <code> real(rk), intent(in) </code> </td>
+  !!   <td>
+  !!   Length of supercell in y direction.
+  !!   </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code> Lz </code> </td>
+  !!   <td> <code> real(rk), intent(in) </code> </td>
+  !!   <td>
+  !!   Length of supercell in z direction.
+  !!   </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code> r </code> </td>
+  !!   <td> <code> real(rk), intent(in), dimension(:,:) </code> </td>
+  !!   <td>
+  !!   Array containing the particle positions. These positions will be within the supercell 'box'.  
+  !!   </td>
+  !!  </tr>
+  !!  <tr>
+  !!   <td> <code>i </code> </td>
+  !!   <td> <code> integer(ik) </code> </td>
+  !!   <td> The particle whose energy is to be calculated. </td>
+  !!  </tr>
+  !! </table>
+  !! <p><b>Returns:</b> <code> real(rk) </code> </p>
   function particle_energy(Lx,Ly,Lz,r,i)
     real(rk), intent(in) :: Lx
     real(rk), intent(in) :: Ly
