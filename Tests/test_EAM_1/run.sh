a498a1c0e896da5e0df9131621eb6c804e19f8ce
Tom Underwood <t.l.underwood@ed.ac.uk>
2015-11-08 22:19:46 +0000

    Added EAM test case corresponding to the example. I have not yet test it though, but it should work.

---
+++ Tests/test_EAM_1/run.sh
@@ -0,0 +1,8 @@
+mpiexec -n 4 ../../monteswitch_mpi -new
+cp state state_AFTER_WF_GEN
+sed -i 's/update_eta=[ \t]*T/update_eta= F/' state
+sed -i 's/update_trans=[ \t]*T/update_trans= F/' state
+sed -i 's/enable_barriers=[ \t]*T/enable_barriers= F/' state
+sed -i 's/stop_sweeps=[ \t]*160000/stop_sweeps= 700000/' state
+mpiexec -n 4 ../../monteswitch_mpi -reset
+
