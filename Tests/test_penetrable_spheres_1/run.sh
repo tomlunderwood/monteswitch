d72dcf3543212b499a7df9f5afcf7f7ccb23c50a
Tom Underwood <t.l.underwood@ed.ac.uk>
2015-11-18 23:29:12 +0000

    Fix bugs in test_penetrable_spheres_1

--- Tests/test_penetrable_spheres_1/run.sh
+++ Tests/test_penetrable_spheres_1/run.sh
@@ -1,8 +1,17 @@
-mpiexec -n 4 ../../monteswitch_mpi -new
+mpiexec -n 4 ../../monteswitch_mpi -new -explicit
 cp state state_AFTER_WF_GEN
-sed -i 's/update_eta=[ \t]*T/update_eta= F/' state
-sed -i 's/update_trans=[ \t]*T/update_trans= F/' state
-sed -i 's/enable_barriers=[ \t]*T/enable_barriers= F/' state
-sed -i 's/stop_sweeps=[ \t]*500000/stop_sweeps= 10000000/' state
-mpiexec -n 4 ../../monteswitch_mpi -reset
+cp state_0 state_0_AFTER_WF_GEN
+cp state_1 state_1_AFTER_WF_GEN
+cp state_2 state_2_AFTER_WF_GEN
+cp state_3 state_3_AFTER_WF_GEN
+cp data_0 data_0_AFTER_WF_GEN
+cp data_1 data_1_AFTER_WF_GEN
+cp data_2 data_2_AFTER_WF_GEN
+cp data_3 data_3_AFTER_WF_GEN
+sed -i '' 's/update_eta=[ \t]*T/update_eta= F/' state
+sed -i '' 's/update_trans=[ \t]*T/update_trans= F/' state
+sed -i '' 's/enable_barriers=[ \t]*T/enable_barriers= F/' state
+sed -i '' 's/stop_sweeps=[ \t]*500000/stop_sweeps= 10000000/' state
+sed -i '' 's/equil_sweeps=[ \t]*0/equil_sweeps= 100000/' state
+mpiexec -n 4 ../../monteswitch_mpi -reset -explicit
 
