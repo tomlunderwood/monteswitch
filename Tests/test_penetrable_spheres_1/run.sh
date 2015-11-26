6da2d1c292f818b2a33e4571b3cac9e89e153ffc
Tom Underwood <t.l.underwood@ed.ac.uk>
2015-11-26 21:04:42 +0000

    Finalised test case 'test_penetrable_spheres_1'

--- Tests/test_penetrable_spheres_1/run.sh
+++ Tests/test_penetrable_spheres_1/run.sh
@@ -11,7 +11,7 @@ cp data_3 data_3_AFTER_WF_GEN
 sed -i '' 's/update_eta=[ \t]*T/update_eta= F/' state
 sed -i '' 's/update_trans=[ \t]*T/update_trans= F/' state
 sed -i '' 's/enable_barriers=[ \t]*T/enable_barriers= F/' state
-sed -i '' 's/stop_sweeps=[ \t]*500000/stop_sweeps= 10000000/' state
-sed -i '' 's/equil_sweeps=[ \t]*0/equil_sweeps= 100000/' state
+sed -i '' 's/stop_sweeps=[ \t]*2000000/stop_sweeps= 25000000/' state
+sed -i '' 's/equil_sweeps=[ \t]*0/equil_sweeps= 250000/' state
 mpiexec -n 4 ../../monteswitch_mpi -reset -explicit
 
