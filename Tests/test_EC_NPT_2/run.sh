7fd587204c06a396a253806f70983ef395f67efc
Tom Underwood <t.l.underwood@bath.ac.uk>
2016-04-02 20:33:25 +0100

    Updated Einstein crystal tests and manual text, including for the NPT Einstein crystal. Note that there were some bugs in the NPT tests which needed correcting.

--- Tests/test_EC_NPT_2/run.sh
+++ Tests/test_EC_NPT_2/run.sh
@@ -1,6 +1,6 @@
-echo "Running simulation 1 of 10"
+echo "Running simulation 1 of 5"
 ../../monteswitch -new
-for i in 2 3 4 5 6 7 8 9 10; do
-  echo "Running simulation $i of 10"
+for i in 2 3 4 5; do
+  echo "Running simulation $i of 5"
   ../../monteswitch -resume
 done
